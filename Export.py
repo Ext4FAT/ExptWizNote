75a6eb9c624b8a3ce46ef8a2ab7847cd7d6d8dd9
_IDLER_ <zyy34472@gmail.com>
2017-07-07 18:30:06 +0800

    replace title to name

--- Export.py
+++ Export.py
@@ -13,7 +13,7 @@ ROOT = './OUT'
 NOTES = './notes/'
 ATTACHMENTS = './attachments/'
 
-PYINTERPRETER = "/usr/bin/python3" 
+PYINTERPRETER = "/usr/bin/python3"
 HTML2TEXT = "./lib/html2text.py"
 
 def unzip(src, dst):
@@ -23,14 +23,15 @@ def unzip(src, dst):
     zip_file.close()
 
 def rmExtname(filename):
+    print(os.path.splitext(filename))
     (shortname, extension) = os.path.splitext(filename)
     return shortname
 
 def makePath(path):
     flag = os.path.exists(path)
-    if not flag: 
+    if not flag:
         os.makedirs(path)
-    return flag 
+    return flag
 
 def readFromDB(dbname, sql):
     mydb = sqlite3.connect(dbname)
@@ -44,7 +45,7 @@ def addInitURL(htmlname):
     html = codecs.open(htmlname, 'rb', 'utf-8')
     soup = BeautifulSoup(html, "lxml")
     body = soup.body
-    a_tag = body.new_tag("a", href = "www.example.com") 
+    a_tag = body.new_tag("a", href = "www.example.com")
     body.insert(0, a_tag)
     newhtml = codecs.open('new-' + htmlname, 'wb', 'utf-8')
     newhtml.write(str(soup))
@@ -58,47 +59,44 @@ def wrapMarkdown(filepath, mdpath):
 
 def copyNotes(table):
     for row in table:
-        hash, title, location, notename, url = row
-        notename = rmExtname(notename)
+        hash, notetitle, location, url = row
         spath = NOTES + '{' + hash + '}'
-        dpath = ROOT + location + notename
-        if notename.endswith('.md'):
+        dpath = ROOT + location + notetitle
+        if notetitle.endswith('.md'):
             dpath = dpath + '.md'
         makePath(dpath)
-        if url:    
+        if url:
             print(url)
             #addInitURL(dpath + '/index.html')
         try:
             unzip(spath, dpath)
             print('[UNZIP]\t' + spath + '->' + dpath)
-            if notename.endswith('.md'):
-                wrapMarkdown(dpath + '/index.html', ROOT + location + notename)
+            if notetitle.endswith('.md'):
+                wrapMarkdown(dpath + '/index.html', ROOT + location + notetitle)
                 shutil.rmtree(dpath)
         except Exception as e:
             print(e)
 
 def copyAttachments(table):
     for row in table:
-        hash, location, notename, attname = row
-        notename = rmExtname(notename)
-        makePath(ROOT + location + notename)
+        hash, location, notetitle, attname = row
+        makePath(ROOT + location + notetitle)
         spath = ATTACHMENTS + '{' + hash + '}' + attname
-        dpath = ROOT + location + notename + '/' + attname
+        dpath = ROOT + location + notetitle + '/' + attname
         try:
             shutil.copyfile(spath, dpath)
             print('[INFO]\t' + spath)
         except Exception as e:
             print(e)
             #print('[ERROR]\t' + spath)
-        
+
 def main():
-    sql_note = "select DOCUMENT_GUID, DOCUMENT_TITLE, DOCUMENT_LOCATION, DOCUMENT_NAME, DOCUMENT_URL from WIZ_DOCUMENT"
-    sql_attach = "select ATTACHMENT_GUID, DOCUMENT_LOCATION, DOCUMENT_NAME, ATTACHMENT_NAME from WIZ_DOCUMENT, WIZ_DOCUMENT_ATTACHMENT where  WIZ_DOCUMENT.DOCUMENT_GUID = WIZ_DOCUMENT_ATTACHMENT.DOCUMENT_GUID"
+    sql_note = "select DOCUMENT_GUID, DOCUMENT_TITLE, DOCUMENT_LOCATION, DOCUMENT_URL from WIZ_DOCUMENT"
+    sql_attach = "select ATTACHMENT_GUID, DOCUMENT_LOCATION, DOCUMENT_TITLE, ATTACHMENT_NAME from WIZ_DOCUMENT, WIZ_DOCUMENT_ATTACHMENT where  WIZ_DOCUMENT.DOCUMENT_GUID = WIZ_DOCUMENT_ATTACHMENT.DOCUMENT_GUID"
     notes = readFromDB("index.db", sql_note)
     copyNotes(notes)
-    '''
     atttachments = readFromDB("index.db", sql_attach)
-    copyAttachments(atttachments)  
-    '''
+    copyAttachments(atttachments)
+
 if __name__ == "__main__":
     main()
